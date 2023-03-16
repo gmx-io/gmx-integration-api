57c087ab9371ee488d8a3ce6b1fe6e687f9208ec
vipin <vipineth@gmail.com>
2023-03-16 22:14:57 +0530

    add config

---
+++ config/constants.ts
@@ -0,0 +1,3 @@
+export const AVALANCHE = 43114
+export const ARBITRUM = 42161
+export const AddressZero = '0x0000000000000000000000000000000000000000'
---
+++ config/subgraphs.ts
@@ -0,0 +1,8 @@
+import { ARBITRUM, AVALANCHE } from './constants'
+
+export const SUBGRAPHS_API_URLS = {
+  [ARBITRUM]:
+    'https://api.thegraph.com/subgraphs/name/vipineth/gmx-stats-arbitrum',
+  [AVALANCHE]:
+    'https://api.thegraph.com/subgraphs/name/vipineth/gmx-stats-avax',
+}
