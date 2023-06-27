428b93f3d1559105792de3e7d524e4e0c1944b46
vipin <vipineth@gmail.com>
2023-06-27 21:02:22 +0530

    update subgraph api url

--- config/constants.ts
+++ config/constants.ts
@@ -4,7 +4,7 @@ export const AddressZero = '0x0000000000000000000000000000000000000000'
 
 export const SUBGRAPHS_API_URLS: { [key: number | string]: string } = {
   [ARBITRUM]:
-    'https://api.thegraph.com/subgraphs/name/vipineth/gmx-stats-arbitrum',
+    'https://subgraph.satsuma-prod.com/3b2ced13c8d9/gmx/gmx-arbitrum-stats/api',
   [AVALANCHE]:
     'https://api.thegraph.com/subgraphs/name/vipineth/gmx-stats-avax',
   chainlink: 'https://api.thegraph.com/subgraphs/name/deividask/chainlink',
