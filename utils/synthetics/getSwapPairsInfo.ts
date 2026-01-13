fa0c0b74ebe36d8c50f38f76ba727d2e8be43de6
Hubért de Lalye <hubert-de-lalye@proton.me>
2026-01-13 13:42:28 +0400

    fixed issues with spot markets, unified api format

--- utils/synthetics/getSwapPairsInfo.ts
+++ utils/synthetics/getSwapPairsInfo.ts
@@ -1,48 +1,54 @@
 import { isSameStr } from '@/lib/index'
-import { get24HSwapVolume } from './getSwapVolumes'
-import { getSwapMarkets } from './getSwapMarkets'
-import { getTokensPrice } from './getTokensPrice'
 import { getMarketsLiquidity } from './getMarketsLiquidityInfo'
+import { getSwapMarkets } from './getSwapMarkets'
+import { get24HSwapVolume } from './getSwapVolumes'
+import { getTokensPrice } from './getTokensPrice'
 
 export async function getSwapPairsInfo(chainId: number) {
   const [swapPairs, pairSwapVolume, prices, liquidityInfo] = await Promise.all([
     getSwapMarkets(chainId),
     get24HSwapVolume(chainId),
     getTokensPrice(chainId),
-    getMarketsLiquidity(chainId)
-  ]);
+    getMarketsLiquidity(chainId),
+  ])
 
   if (!swapPairs || !pairSwapVolume || !prices || !liquidityInfo) {
-    return null;
+    return null
   }
 
   return swapPairs?.map((pair) => {
-    const { longToken, shortToken, longTokenInfo, shortTokenInfo, marketToken } = pair
+    const {
+      longToken,
+      shortToken,
+      longTokenInfo,
+      shortTokenInfo,
+      marketToken,
+    } = pair
     const pairAddress = `${longToken}-${shortToken}`
     const liquidityUsd = liquidityInfo[marketToken]?.liquidityUsd ?? 0n
-    const volumeUsd = pairSwapVolume?.[pairAddress.toLowerCase()] ?? 0
+    const volumeUsd = pairSwapVolume?.[pairAddress] ?? 0
     const longTokenSymbol = longTokenInfo.baseSymbol ?? longTokenInfo.symbol
     const shortTokenSymbol = shortTokenInfo.baseSymbol ?? shortTokenInfo.symbol
-    const longTokenPriceInfo = prices.find((price) => isSameStr(price.tokenSymbol, longTokenSymbol))
-    const shortTokenPriceInfo =  prices.find((price) => isSameStr(price.tokenSymbol, shortTokenSymbol))
-    const priceCalculation = (type: 'close' | 'high' | 'low') =>
-      longTokenPriceInfo && shortTokenPriceInfo
-        ? (longTokenPriceInfo[type] ?? 0) / (shortTokenPriceInfo[type] ?? 1)
-        : 0
+    const longTokenPriceInfo = prices.find((price) =>
+      isSameStr(price.tokenSymbol, longTokenSymbol)
+    )
+    const shortTokenPriceInfo = prices.find((price) =>
+      isSameStr(price.tokenSymbol, shortTokenSymbol)
+    )
 
     return {
       ticker_id: `${longTokenSymbol}-${shortTokenSymbol}`,
       base_currency: longTokenSymbol,
       target_currency: shortTokenSymbol,
       product_type: 'Spot',
-      last_price: priceCalculation('close'),
-      high: priceCalculation('high'),
-      low: priceCalculation('low'),
+      last_price: longTokenPriceInfo?.close ?? 0,
+      high: longTokenPriceInfo?.high ?? 0,
+      low: longTokenPriceInfo?.low ?? 0,
       base_volume: volumeUsd / (longTokenPriceInfo?.close ?? 1),
       target_volume: volumeUsd / (shortTokenPriceInfo?.close ?? 1),
       volume_usd: volumeUsd,
       pool_id: marketToken,
-      liquidity_in_usd: liquidityUsd
+      liquidity_in_usd: Number(liquidityUsd),
     }
   })
 }
